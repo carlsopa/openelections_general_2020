2529f286f48e03ec583b20ba16195ced3808f38a
Paul R. Carlson <carlsonpar@gmail.com>
2021-01-01 13:51:26 -0500

    fixed column headers and naming conventions.

--- mi/van_buren/van_buren.py
+++ mi/van_buren/van_buren.py
@@ -1,5 +1,6 @@
 import tabula
 import pandas as pd
+import numpy as np
 
 new = True
 pdf_file = 'Van Buren MI Results per Precinct Data report.pdf'
@@ -7,10 +8,34 @@ pdf_file = 'Van Buren MI Results per Precinct Data report.pdf'
 race_name = 'President/Vice Pres'
 precinct = []
 candidate = []
+district = []
 votes = []
 race = []
 start_page = 1
 end_page = 5
+
+#function that takes a race name and then will split apart any district numbers, which will then be placed into their own column
+def local_district(race):
+    index = 1
+    integer = True
+    district_numbers = []
+    result = 0
+    c = ''
+    if 'th' in race:
+        race = race[:race.find('th')]
+        while integer:
+            try:
+                district_numbers.append(int(race[-index]))
+                index = index + 1
+            except:
+                integer = False
+        for x in district_numbers:
+            c = c+str(x)
+        result = [race[:-index+1],c]
+    else:
+        result = [race,np.nan]
+    return(result)
+
 while start_page <= end_page:
     data = tabula.read_pdf(pdf_file,multiple_tables=True,lattice=False,stream=False,pages=(start_page))
     new = False
@@ -26,7 +51,8 @@ while start_page <= end_page:
                         candidate.append(df.columns.values[i])
                         votes.append(df.iloc[index,i])
                         precinct.append(df.iloc[index,0])
-                        race.append(race_name)
+                        district.append(local_district(race_name)[1])
+                        race.append(local_district(race_name)[0])
         else:
             df.drop(df.columns[0],axis=1,inplace=True)
             #an array of row indexes to skip and not process.
@@ -61,7 +87,8 @@ while start_page <= end_page:
                                 if race_name == 'State Board of Education':
                                     pass
                                 else:
-                                    race.append(race_name)
+                                    district.append(local_district(race_name)[1])
+                                    race.append(local_district(race_name)[0])
                                     #several pages combined the results into one column, this will split that into two columns and store the data correctly.
                                     if ' ' in str(df.iloc[index,i]):
                                         votes.append(df.iloc[index,i].split(' ')[0])
@@ -69,7 +96,8 @@ while start_page <= end_page:
                                         candidate.append(candidateList[-2])
                                         candidate.append(candidateList[-1])
                                         precinct.append(df.iloc[index,0])
-                                        race.append(race_name)
+                                        district.append(local_district(race_name)[1])
+                                        race.append(local_district(race_name)[0])
                                     else:
                                         votes.append(df.iloc[index,i])
                                         candidate.append(candidateList[i-1])
@@ -81,6 +109,6 @@ while start_page <= end_page:
 
     start_page = start_page+1
 
-final = {'precinct':precinct,'race':race,'candidate':candidate,'votes':votes}
+final = {'Precinct':precinct,'Office':race,'District':district,'Candidate':candidate,'Votes':votes}
 new = pd.DataFrame(final)
 new.to_csv('20201103_mi_general_van_buren_precinct.csv')
